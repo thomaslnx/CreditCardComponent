e5579b0a29dcb1cfce561cf5853278ba1fabce89
thomaslnx <marcos.msilva10@gmail.com>
2021-08-30 11:03:49 -0300

    adding SEO to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps, NextWebVitalsMetric } from 'next/app'
 import { ChakraProvider, CSSReset } from '@chakra-ui/react'
 import { ReactElement } from 'react'
+import { DefaultSeo } from 'next-seo'
 
 import creditCardTheme from '../styles'
 
@@ -9,7 +10,23 @@ function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return (
    <ChakraProvider theme={creditCardTheme}>
      <CSSReset />
-     <Component {...pageProps} />
+     <DefaultSeo
+      openGraph={{
+        title: 'Animated Credit Card Component',
+        description: 'PoC from app using NextJS and ChakraUI',
+        type: 'website',
+        locale: 'en_IE',
+        url: 'https://credit-card-component.vercel.app/',
+        site_name: 'credit card component'
+      }}
+      twitter={{
+        handle: '@thomaslnx',
+        site: '@thomaslnx',
+        cardType: 'sumary_large_image'
+      }}
+     >
+        <Component {...pageProps} />
+     </DefaultSeo>
    </ChakraProvider>
   )
 }
