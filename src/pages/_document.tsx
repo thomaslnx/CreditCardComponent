f27c77f6841635410b196c9b0d0c6c10725752ed
thomaslnx <marcos.msilva10@gmail.com>
2021-08-30 16:17:16 -0300

    changing deploy sever and google verification tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <meta name="google-site-verification" content="B8D5ApB32yGPbtbxP6utFKDXPh-pTHR5qpaVfz0a1QA" />
+          <meta name="google-site-verification" content="Gx1EvYN_thT7bHiq0b2kVFiocB6RbTetFoemmA1hG-8" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;500;600;700&display=swap" />
